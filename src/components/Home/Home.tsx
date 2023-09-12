0650526793e0c414fb267fbdcfb460a0a03035cf
Alejo Holmann <alejoholmann99@gmail.com>
2023-09-12 01:11:53 -0300

    correcciones home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,30 +7,31 @@ import Image from "next/image";
 const Home = () => {
   /*<div className="min-h-screen">*/
   return (
-    <div className={style.alejoback}>
-      <Navbar />
-      <div className={style.container}>
-        <div className={style.conth1}>
-          <h1 className={style.h1}>
-            {" "}
-            ¡Hola!, <br />
-            soy Alejo Holmann <br />
-            <span className={style.span}>
-              <span className={style.span}> Eficiente</span>
-              <span className={style.span}> Desarrollador Web</span>
-              <span className={style.span}> Creativo</span>
-              <span className={style.span}> Proactivo</span>
-              <span className={style.span}> Responsable</span>
-            </span>
-          </h1>
-        </div>
-        {/*
+    <>
+      <div className={style.alejoback}>
+        <Navbar />
+        <div className={style.container}>
+          <div className={style.conth1}>
+            <h1 className={style.h1}>
+              {" "}
+              ¡Hola!, <br />
+              soy Alejo Holmann <br />
+              <span className={style.span}>
+                <span className={style.span}> Eficiente</span>
+                <span className={style.span}> Desarrollador Web</span>
+                <span className={style.span}> Creativo</span>
+                <span className={style.span}> Proactivo</span>
+                <span className={style.span}> Responsable</span>
+              </span>
+            </h1>
+          </div>
+
           <div className={style.imgcontainer}>
             <Image className={style.mainImage} src={ale} alt="Alejo Holmann" />
           </div>
-  */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
