3311af4071f8b1107b2d3812f2f420e857766ce3
Alejo Holmann <alejoholmann99@gmail.com>
2023-09-18 01:22:55 -0300

    base about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,21 +2,25 @@ import style from "./about.module.css";
 
 const About = () => {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen ">
       <div className={style.main}>
-        <aside>
-          <article>
-            <h2>Desarrollador Web Full Stack</h2>
-            <p>Mi nombre es </p>
-            <h3>Alejo Holmann. </h3>
-            <p>
-              Oriundo de Córdoba, a mis 23 años me dedico al desarrollo web de
-              aplicaciones dinámicas. Me enfoco en darles un {"toque"}{" "}
-              minimalista, una buena experiencia de usuario y un diseño acorde a
-              las necesidades.
-            </p>
-          </article>
-        </aside>
+        <div className={style.back}>
+          <section className={style.card}>
+            <div>
+              <h2>Desarrollador Web Full Stack</h2>
+              <p>Mi nombre es </p>
+              <h3>Alejo Holmann. </h3>
+            </div>
+            <article>
+              <p>
+                Oriundo de Córdoba, a mis 23 años me dedico al desarrollo web de
+                aplicaciones dinámicas. Me enfoco en darles un {"toque"}{" "}
+                minimalista, una buena experiencia de usuario y un diseño acorde
+                a las necesidades.
+              </p>
+            </article>
+          </section>
+        </div>
       </div>
     </div>
   );
