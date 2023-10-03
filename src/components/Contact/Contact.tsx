3644d4481612b487cefdc366145a7201aada2648
Alejo Holmann <alejoholmann99@gmail.com>
2023-10-03 03:27:14 -0300

    contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen">
+    <div className={style.container}>
       <h2>Contáctame</h2>
       <h4>
         Si tienes interés en <b>contratarme</b>, házmelo saber, <b>envíame</b>{" "}
@@ -12,7 +12,11 @@ const Contact = () => {
 
       <div className={style.card}>
         <div>
-          <Link href={"mailto:alejoholmann99@gmail.com"} target="_blank">
+          <Link
+            href={"mailto:alejoholmann99@gmail.com"}
+            target="_blank"
+            className={style.link}
+          >
             <svg
               viewBox="0 0 32 32"
               data-name="Layer 1"
@@ -52,13 +56,14 @@ const Contact = () => {
                 ></path>
               </g>
             </svg>
-            <p>alejoholmann99@gmail.com</p>
+            <h5>alejoholmann99@gmail.com</h5>
           </Link>
         </div>
         <div>
           <Link
             href={"https://www.linkedin.com/in/alejo-holmann-a51262221/"}
             target="_blank"
+            className={style.link}
           >
             <svg
               fill="#0077B5"
@@ -93,11 +98,16 @@ const Contact = () => {
                 </g>{" "}
               </g>
             </svg>
-            <p>Alejo Holmann</p>
+
+            <h5>Alejo Holmann</h5>
           </Link>
         </div>
         <div>
-          <Link href={"https://github.com/AIHolmann"} target="_blank">
+          <Link
+            href={"https://github.com/AIHolmann"}
+            target="_blank"
+            className={style.link}
+          >
             <svg
               width="35"
               height="35"
@@ -109,7 +119,7 @@ const Contact = () => {
                 d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
               />
             </svg>
-            <p>AIHolmann</p>
+            <h5>AIHolmann</h5>
           </Link>
         </div>
       </div>
