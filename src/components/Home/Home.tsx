75a238f4f160cf923efaa5bcb1fac4085fcaa5c0
Alejo Holmann <alejoholmann99@gmail.com>
2024-06-13 15:42:34 -0300

    end responsive

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import Navbar from "../Navbar/Navbar";
 import Arrowdown from "./arrowdown/Arrowdown";
 import Cardcounter from "./cardcounter/Cardcounter";
